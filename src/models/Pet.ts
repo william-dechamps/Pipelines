f29c5968e476d5a9220ffcf43fa020bd23a2d4b0
William <william-dechamps@hotmail.com>
2023-03-31 14:46:15 +0200

    Add User and Pet methods

--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -1,28 +1,28 @@
-import JSONable from "./JSONable"
+import JSONable from "./JSONable";
 
 export default class Pet implements JSONable {
-  id?: any
-  vetId: Number
-  name: string
-  kind: string
-  birthDate: Date
+  id?: any;
+  vetId: Number;
+  name: string;
+  kind: string;
+  birthDate: Date;
   constructor(vetId: Number, name: string, kind: string, birthDate: Date) {
-    this.vetId = vetId
-    this.name = name
-    this.kind = kind || ""
-    this.birthDate = birthDate
+    this.vetId = vetId;
+    this.name = name;
+    this.kind = kind || "";
+    this.birthDate = birthDate;
   }
 
   /**
    * @returns Number the current pet's age calculated from its birthDate
    */
   getAge(): number {
-    const today = new Date()
-    const age = today.getFullYear() - this.birthDate.getFullYear()
+    const today = new Date();
+    const age = today.getFullYear() - this.birthDate.getFullYear();
     if (today.getMonth() === this.birthDate.getMonth()) {
-      return age - (today.getDate() > this.birthDate.getDate() ? 0 : 1)
+      return age - (today.getDate() > this.birthDate.getDate() ? 0 : 1);
     }
-    return age - (today.getMonth() > this.birthDate.getMonth() ? 0 : 1)
+    return age - (today.getMonth() > this.birthDate.getMonth() ? 0 : 1);
   }
 
   /**
@@ -30,11 +30,11 @@ export default class Pet implements JSONable {
    * @example "Pastèque (123456): dog, 2"
    */
   describe(): string {
-    throw new Error("Not implemented")
+    return `${this.name} (${this.vetId}): ${this.kind}, ${this.getAge()}`;
   }
 
   /**
-   * 
+   *
    * @returns a JSON representation of the current Pet
    * @example
    * ```json
@@ -44,12 +44,12 @@ export default class Pet implements JSONable {
    *    age: 2
    * }
    * ```
-   */toJSON(): Object {
+   */ toJSON(): Object {
     return {
       vetId: this.vetId,
       name: this.name,
       kind: this.kind,
-      age: this.getAge()
-    }
+      age: this.getAge(),
+    };
   }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,22 @@
-import JSONable from "./JSONable"
-import Pet from "./Pet"
+import JSONable from "./JSONable";
+import Pet from "./Pet";
 
 export default class User implements JSONable {
-  id?: any
-  firstname: string
-  lastname: string
-  pets: string[]
-  constructor(firstname: string, lastname: string) {
-    this.firstname = firstname
-    this.lastname = lastname
-    this.pets = []
+  id?: any;
+  firstname: string;
+  lastname?: string;
+  pets: string[];
+  constructor(firstname: string, lastname?: string) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.pets = [];
   }
 
   /**
    * @returns string `firstname lastname`
    */
   getFullname(): string {
-    throw new Error("Not implemented")
+    return `${this.firstname} ${this.lastname}`;
   }
 
   /**
@@ -24,11 +24,11 @@ export default class User implements JSONable {
    * @param pet Pet to add to the list
    */
   addPet(pet: Pet) {
-    throw new Error("Not implemented")
+    this.pets.push(pet.vetId.toString());
   }
 
   /**
-   * 
+   *
    * @returns a JSON representation of the current User
    * @example
    * ```json
@@ -43,7 +43,9 @@ export default class User implements JSONable {
    */
   toJSON() {
     return {
-      pets: this.pets
-    }
+      firstname: this.firstname,
+      lastname: this.lastname,
+      pets: this.pets,
+    };
   }
 }
